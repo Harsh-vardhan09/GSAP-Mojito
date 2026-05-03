cd0dbb466d95dfb80b9f150c2c0b8ec7bf8a642b
Harsh Vardhan <harsh.vardhanp0901@gmail.com>
2026-05-03 22:56:50 +0530

    Art

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,24 @@
-import React from 'react'
-import { ScrollTrigger,SplitText } from 'gsap/all'
-import gsap from 'gsap'
-import Navbar from './components/Navbar'
-import Hero from './components/Hero'
-import About from './components/About'
-import Cocktails from './components/Cocktails'
+import React from "react";
+import { ScrollTrigger, SplitText } from "gsap/all";
+import gsap from "gsap";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import About from "./components/About";
+import Cocktails from "./components/Cocktails";
+import Art from "./components/Art";
 
-gsap.registerPlugin(ScrollTrigger,SplitText)
+gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const App = () => {
   return (
     <main>
-        <Navbar/>
-        <Hero/>
-        <Cocktails/>
-        <About/>
-    
+      <Navbar />
+      <Hero />
+      <Cocktails />
+      <About />
+      <Art />
     </main>
-  )
-}
+  );
+};
 
-export default App
+export default App;
